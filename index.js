6c544e3faed57a29403e7b07c82c0f27b7de9ecb
Phelipe Maglio <phelipe.maglio@dextra-sw.com>
2021-04-16 16:59:55 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ Dado um cheque com o seu valor por extenso, desenvolva um programa que retorne o
 Por exemplo se informarmos "dois mil quinhentos e vinte e três reais e dezoito centavos", o seu programa deve retornar "2523,18"
 */
 
+let inputNumber = 'dois mil quinhentos e vinte e três reais e dezoito centavos';
+let splitString = split.inputNumber(' ');
 
+let dictionary = {
+    'zero': 0
+}
