2d2187a4d6b579d8fc023c2f6ed20e45ddb9a77d
Josiene Vieira Nascimento Dourado <josiene.dourado@dft-sp-wkn1214.dafiti.local>
2021-04-16 17:13:00 -0300

    h

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ Dado um cheque com o seu valor por extenso, desenvolva um programa que retorne o
 Por exemplo se informarmos "dois mil quinhentos e vinte e três reais e dezoito centavos", o seu programa deve retornar "2523,18"
 */
 
+const dictionaryUnidade = {
+  'zero': 0,
+  'um': 1,
+  'dois': 2,
+  'tres': 3,
+  'quatro': 4,
+  'cinco': 5,
+  'seis': 6,
+  'sete': 7,
+  'oito': 8,
+  'nove': 9,
+}
+
+const dictionaryDezena = {
+  'dez': 10,
+  'onze': 11,
+  'doze': 12,
+  'treze': 13,
+  'catorze': 14,
+  'quinze': 15,
+  'dezesseis': 16,
+  'dezessete': 17,
+  'desoito': 18,
+  'dezenove': 19
+} 
+
 let inputNumber = 'dois mil quinhentos e vinte e três reais e dezoito centavos';
 let splitString = split.inputNumber(' ');
 
-let dictionary = {
-    'zero': 0
-}
+
